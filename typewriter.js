770f51be259f5051ac1bdb0390610324af604e59
Rohit <rohitcricbuzz@gmail.com>
2021-01-12 17:53:44 +0000

    typewriter js revised

--- typewriter.js
+++ typewriter.js
@@ -1,7 +1,12 @@
 const sentence = "hello there from lighthouse labs";
 for (let i = 0; i < sentence.length; i++) {
-    setTimeout(() => {
-        process.stdout.write(sentence[i]);
-    }, i*1000)
+    if (i === sentence.length - 1) {
+        setTimeout(() => {
+            process.stdout.write(`${sentence[i]}\n`);
+        }, i*10)
+    } else {
+        setTimeout(() => {
+            process.stdout.write(sentence[i]);
+        }, i*10)
+    } 
 }
-process.stdout.write('\n');
